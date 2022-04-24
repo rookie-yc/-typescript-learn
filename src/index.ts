a64d61f5291f6486c5993a9182b7906973c37ad3
yinchen <yinchen@zhongan.com>
2022-04-25 04:00:55 +0800

    feat:贪吃蛇demo完成

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import './style/index.less';
+import GameControl from "./modules/GameControl";
+new GameControl();
+
+
+
---
+++ src/modules/GameControl.ts
@@ -0,0 +1,75 @@
+import Food from "./Food";
+import Snake from "./Snake";
+import ScorePanel from "./ScorePanel";
+
+class GameControl {
+    snake: Snake;
+    food: Food;
+    scorePanel: ScorePanel;
+
+    direction: string = 'ArrowRight';
+    isLive = true;
+
+    constructor() {
+        this.snake = new Snake();
+        this.food = new Food();
+        this.scorePanel = new ScorePanel(10, 1);
+        this.init();
+    }
+
+    init() {
+        document.addEventListener('keydown',  this.keydownHandler.bind(this));
+        this.run();
+    }
+
+    keydownHandler(event: KeyboardEvent) {
+        // console.log(event.key)
+        this.direction = event.key;        
+    }
+
+    run() {
+        let X = this.snake.X;
+        let Y = this.snake.Y;
+
+        switch (this.direction) {
+            case "ArrowUp":
+            case "Up":
+                Y -= 10;
+                break;
+            case "ArrowDown":
+            case "Down":
+                Y += 10;
+                break;
+            case "ArrowLeft":
+            case "Left":
+                X -= 10;
+                break;
+            case "ArrowRight":
+            case "Right":
+                X += 10;
+                break;
+        }
+
+        this.checkEat(X, Y);
+
+        try {
+            this.snake.X = X;
+            this.snake.Y = Y;
+        } catch (e:any) {
+            alert(e.message + ' GAME OVER!');
+            this.isLive = false;
+        }
+        this.isLive && setTimeout(this.run.bind(this), 300 - (this.scorePanel.level - 1) * 30);
+    }
+
+    checkEat(X: number, Y: number) {
+        if(X === this.food.X && Y === this.food.Y) {
+            this.food.change();
+            this.scorePanel.addScore();
+            this.snake.addBody()
+        }
+    }
+
+}
+
+export default GameControl;
